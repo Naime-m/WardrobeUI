8137ff4481bb6de226ac323c3468fc57e5796f85
Naime Medjek <naimemedjek@gmail.com>
2023-01-04 10:16:46 +0100

    added create route to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GarmentsCreateComponent } from './garments-create/garments-create.component';
 import { GarmentsEditComponent } from './garments-edit/garments-edit.component';
 import { GarmentsComponent } from './garments/garments.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo:'index'},
   {path: 'index', component: GarmentsComponent},
-  {path: 'edit/:id', component:GarmentsEditComponent}
+  {path: 'edit/:id', component:GarmentsEditComponent},
+  {path: 'create', component:GarmentsCreateComponent}
 ];
 
 @NgModule({
