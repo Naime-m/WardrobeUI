25cfdfbfaa51ee71b2b72e6740b3c48029649516
Naime Medjek <naimemedjek@gmail.com>
2023-01-04 09:48:15 +0100

    imported forms module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { GarmentsComponent } from './garments/garments.component';
 import {HttpClientModule} from '@angular/common/http';
 import { GarmentsEditComponent } from './garments-edit/garments-edit.component';
 import { GarmentsCreateComponent } from './garments-create/garments-create.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     GarmentsComponent,
     GarmentsEditComponent,
-    GarmentsCreateComponent
+    GarmentsCreateComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/garments-edit/garments-edit.component.ts
+++ src/app/garments-edit/garments-edit.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GarmentService } from '../services/garment.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Garment } from '../models/garment';
 
 @Component({
   selector: 'app-garments-edit',
   templateUrl: './garments-edit.component.html',
   styleUrls: ['./garments-edit.component.css']
 })
-export class GarmentsEditComponent {
+export class GarmentsEditComponent implements OnInit {
 
+  garment: any = {};
+
+  constructor(
+    private garmentService: GarmentService,
+    private actRoute: ActivatedRoute) {}
+
+  id = this.actRoute.snapshot.params['id'];
+
+ ngOnInit(): void {
+  this.garmentService.onGetGarment(this.id).subscribe(
+    response => {
+      this.garment = response;
+    }
+  )
+ }
 }
