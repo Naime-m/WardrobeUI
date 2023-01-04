2a78bfe4a2baa867fd35495a0161bce574df738e
Naime Medjek <naimemedjek@gmail.com>
2023-01-04 08:11:46 +0100

    generated garment component

---
+++ src/app/garments/garments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-garments',
+  templateUrl: './garments.component.html',
+  styleUrls: ['./garments.component.css']
+})
+export class GarmentsComponent {
+
+}
