520643ff877e4fb99343bc09a708bfa6c49103a0
Naime Medjek <naimemedjek@gmail.com>
2023-01-04 08:34:23 +0100

    added getallgarments method with httpclient

--- src/app/services/garment.service.ts
+++ src/app/services/garment.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Garment } from '../models/garment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GarmentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  apiUrl = 'https://localhost:7056/api/';
+
+  getAllGarments(): Observable<Garment[]> {
+    return this.http.get<Garment[]>(this.apiUrl);
+  }
 }
